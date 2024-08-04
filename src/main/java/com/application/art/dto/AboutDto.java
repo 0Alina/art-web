5d80f335b30c1ab7e5e0a941422442a494de6157
Dumitras <dumitrufrimu118@gmail.com>
2024-08-04 18:25:46 +0300

    Am modificat pagina de about, pentru a putea edita detaliile si de a fi salvate in baza de date

---
+++ src/main/java/com/application/art/dto/AboutDto.java
@@ -0,0 +1,24 @@
+package com.application.art.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AboutDto {
+
+    private int id = 1;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String phone;
+
+    private String description;
+
+}
---
+++ src/main/java/com/application/art/repository/AboutRepository.java
@@ -0,0 +1,7 @@
+package com.application.art.repository;
+
+import com.application.art.entity.About;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AboutRepository extends JpaRepository<About, Integer> {
+}
